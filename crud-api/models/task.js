2e03a76cec0998ed29a9055f851b4a51d3c7e82b
Rayzr1337 <codehacks76@gmail.com>
2026-07-17 02:26:39 +0300

    Stage 2: add sample tasks

--- crud-api/models/task.js
+++ crud-api/models/task.js
@@ -1,5 +1,9 @@
-let tasks = [];
-let nextId = 1;
+let tasks = [
+  { id: 1, title: 'Buy groceries', description: 'Milk, eggs, bread', completed: false },
+  { id: 2, title: 'Walk the dog', description: 'Around the park', completed: true },
+  { id: 3, title: 'Write code', description: 'Finish CRUD API', completed: false },
+];
+let nextId = 4;
 
 function getAll() {
   return tasks;
